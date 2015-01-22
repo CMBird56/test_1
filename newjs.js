00d70508cad102f880d42bb28bc06c004f83d31c
Philip <filkos56@gmail.com>
2015-01-22 01:20:34 +0100

    Dodat XML fajl

--- newjs.js
+++ newjs.js
@@ -1,3 +1,16 @@
 window.onload = function(){
 	$("#majorDiv").accordion();
+	getData();
+
+function getData(){
+
+	$.get("xml.xml", function(result){
+		var tel = result.getElementsByTagName("phone")[0];
+		var name = result.getElementsByTagName("name")[0];
+		var rezult = "telefon: " + tel.firstChild.nodeValue + " ime: " + name.firstChild.nodeValue ;
+
+		$("#nestoDrugo").append(rezult);
+	});
+}
+
 };
